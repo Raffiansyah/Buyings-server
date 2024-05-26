c843ce55b5c3e39b4d76d96ef351a8a6b039047a
Mohammad Raffiansyah <mohammad.raffiansyah007@gmail.com>
2024-05-26 18:49:59 +0700

    FIx: Fixxing middleware again :)

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.headers?.authorization?.split(' ')[1];
     const supabaseKey = process.env.JWT_SECRET_KEY;
     if (token) {
       jwt.verify(token, supabaseKey);
--- src/middleware/role-middleware.js
+++ src/middleware/role-middleware.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const roleMiddleware = async (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.headers?.authorization?.split(' ')[1];
   const supabaseKey = process.env.JWT_SECRET_KEY;
   try {
     if (token) {
